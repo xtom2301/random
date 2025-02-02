9ae69361c3ccaf51cf31e604bd790c9b3f645013
tmartin <xtom2301@gmail.com>
2025-02-02 14:51:37 +0100

    test

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="rotate-animation">
+        <img src="src/assets/sprite.png" alt="Sprite" className="w-40 h-40" />
+      </div>
+
+      <div className="rotate-animation-reverse">
+        <img src="src/assets/Fuzetea.png" alt="Fuzetea" className="w-40 h-40" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
